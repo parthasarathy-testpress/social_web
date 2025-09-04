997e5c3a4a451b3c42e73de1fafd425f322c2975
parthasarathy-testpress <parthasarathy@testpress.in>
2025-09-04 18:07:21 +0530

    feat: Add profiles for users

--- account/urls.py
+++ account/urls.py
@@ -10,6 +10,8 @@ urlpatterns = [
 
     path('register/', views.register, name='register'),
 
+    path('edit/', views.edit, name='edit'),
+
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 
