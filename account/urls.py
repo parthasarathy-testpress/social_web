ccc53e215ba0f9b9b112484fd015aa03048d4432
parthasarathy AK <parthasarathy@testpress.in>
2025-09-11 12:13:46 +0000

    feat: Add follow unfollow functionality using AJAX

--- account/urls.py
+++ account/urls.py
@@ -24,6 +24,8 @@ urlpatterns = [
     path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(),name='password_reset_confirm'),
     path('reset/done/',auth_views.PasswordResetCompleteView.as_view(),name='password_reset_complete'),
     
+    path('users/follow/', views.user_follow, name='user_follow'),
+    
     path('users/', views.user_list, name='user_list'),
     path('users/<username>/', views.user_detail, name='user_detail'),
 ]
--- account/views.py
+++ account/views.py
@@ -10,6 +10,10 @@ from django.contrib.auth.models import User
 from .forms import LoginForm, ProfileEditForm, UserEditForm, UserRegistrationForm
 from .models import Profile
 
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
+from bookmarks.common.decorators import ajax_required
+from .models import Contact
 
 def user_login(request):
     if request.method == 'POST':
@@ -100,3 +104,27 @@ def user_detail(request, username):
     return render(
         request, "account/user/detail.html", {"section": "people", "user": user}
     )
+
+@ajax_required
+@require_POST
+@login_required
+def user_follow(request):
+    user_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if user_id and action:
+        try:
+            user = User.objects.get(id=user_id)
+            if action == 'follow':
+                Contact.objects.get_or_create(
+                    user_from=request.user,
+                    user_to=user
+                )
+            else:
+                Contact.objects.filter(
+                    user_from=request.user,
+                    user_to=user
+                ).delete()
+            return JsonResponse({'status': 'ok'})
+        except User.DoesNotExist:
+            return JsonResponse({'status': 'error'})
+    return JsonResponse({'status': 'error'})
